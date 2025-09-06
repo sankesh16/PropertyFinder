d20010501bb2f462e2d25a4c7ac2abfd593b4b7f
sankesh <sankeshwaranmohanraj@gmail.com>
2025-09-06 17:59:27 +0530

    updated script to scrape detail page

--- propertyfinder/propertyfinder/spiders/property_finder.py
+++ propertyfinder/propertyfinder/spiders/property_finder.py
@@ -25,7 +25,7 @@ class PropertyFinder(scrapy.Spider):
     def parse(self, response):
         locations = response.css('.cjNmsL h4 a::attr(href)').extract()
         for location in locations:
-            category = ['sale', 'rent']
+            category = ['rent','sale']
             for cat in category:
                 if cat == 'sale':
                     url = f'https://www.onthemarket.com/for-sale{location}?under-offer=true'
@@ -33,9 +33,11 @@ class PropertyFinder(scrapy.Spider):
                 elif cat == 'rent':
                     url = f'https://www.onthemarket.com/to-rent{location}?under-offer=true'
 
-                yield scrapy.Request(url=url, headers=self.headers, callback=self.pagination)
+                yield scrapy.Request(url=url, meta={'category':cat,'location':location.replace('/property','').replace('/','').title()}, headers=self.headers, callback=self.pagination)
     
     def pagination(self, response):
+        location = response.meta['location']
+        category = response.meta['category']
         total_pages = 0
         total_pages = response.css('#pagination-controls ul li:last-child a::text').get()
         if total_pages:
@@ -43,19 +45,85 @@ class PropertyFinder(scrapy.Spider):
             if total_pages > 0:
                 for page in range(1, total_pages+1):
                     url = response.url + f'&page={page}'
-                    yield scrapy.Request(url=url, headers=self.headers, callback=self.list_page)
+                    yield scrapy.Request(url=url, meta={'category':category,'location':location}, headers=self.headers, callback=self.list_page)
 
 
     def list_page(self, response):
+        location = response.meta['location']
+        category = response.meta['category']
         property_list = response.css('.otm-PropertyCardMedia div:nth-child(1) a::attr(href)').extract()
         for property in property_list:
             url = 'https://www.onthemarket.com'+property
-            yield scrapy.Request(url=url, headers=self.headers, callback=self.detail_page)
+            yield scrapy.Request(url=url, meta={'category':category,'location':location}, headers=self.headers, callback=self.detail_page)
 
 
     def detail_page(self, response):
-        breakpoint()
+        location = response.meta['location']
+        category = response.meta['category']
+
+        property_data = {
+            "property_description": None,
+            "labelText": None,
+            "property_type": None,
+            "bedroom": None,
+            "bathroom": None,
+            "address": None,
+            "price": None,
+            "property_title": None,
+            "property_id": None,
+            "lat": None,
+            "long": None,
+            "property_sub_type": None,
+            "epc_rating": None,
+            "property_features": None,
+            "property_rooms": None,
+            "property_date": None,
+            "property_url": None,
+            "property_image": None,
+            "property_location": None,
+            "property_category": None,
+            "agent_name": None,
+            "agent_telephone": None,
+            "agent_logo": None
+        }
+
         # continue
         script_data = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
         data = json.loads(script_data)
-        print()
+
+        property_data['property_description'] =  data['props']['initialReduxState']['property']['description']
+        # labelText = None
+        if 'labelText' in data['props']['initialReduxState']['property']:
+            property_data['labelText'] = data['props']['initialReduxState']['property']['labelText']
+        property_data['property_type'] = data['props']['initialReduxState']['property']['humanisedPropertyType']
+        property_data['bedroom'] = data['props']['initialReduxState']['property']['bedrooms']
+        property_data['bathroom'] = data['props']['initialReduxState']['property']['bathrooms']
+        property_data['address'] = data['props']['initialReduxState']['property']['displayAddress']
+        property_data['price'] = data['props']['initialReduxState']['property']['priceRaw']
+        property_data['property_title'] = data['props']['initialReduxState']['property']['propertyTitle']
+        property_data['property_id'] = data['props']['initialReduxState']['property']['id']
+        property_data['lat'] = data['props']['initialReduxState']['property']['location']['lat']
+        property_data['long'] = data['props']['initialReduxState']['property']['location']['lon']
+        property_data['property_sub_type'] = data['props']['initialReduxState']['property']['propSubId']
+
+        if 'epc' in data['props']['initialReduxState']['property']:
+            if 'rating' in data['props']['initialReduxState']['property']['epc']:
+                property_data['epc_rating'] = data['props']['initialReduxState']['property']['epc']['rating']
+                
+        property_data['property_features'] = [k['feature'] for k in data['props']['initialReduxState']['property']['features']]
+        
+        property_rooms = {k['name']:k['sizeText'] for k in data['props']['initialReduxState']['property']['rooms']['descriptions']}
+        if property_rooms:
+            property_data['property_rooms'] = property_rooms
+
+        property_data['property_date'] = data['props']['initialReduxState']['property']['daysSinceAddedReduced']
+        property_data['property_url'] = response.url
+        property_data['property_image'] = [image['largeUrl'] for image in data['props']['initialReduxState']['property']['images']]
+        property_data['property_location'] = location
+        property_data['property_category'] = category
+
+        property_data['agent_name'] = data['props']['initialReduxState']['agent']['name']
+        property_data['agent_telephone'] = data['props']['initialReduxState']['agent']['telephone']
+        property_data['agent_logo'] = "https://media.onthemarket.com" + data['props']['initialReduxState']['agent']['displayLogo']['url']
+
+        yield property_data
